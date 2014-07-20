657ecb41a3ad8f2a23f94d77b4dfe38d4084e6fc
bite <mlisbit@gmail.com>
2014-07-20 11:52:12 -0400

    search in URL, done with /course/ GET

--- app.py
+++ app.py
@@ -32,31 +32,45 @@ def delete_courses():
 
 @app.route('/courses/', methods = ['GET'])
 def get_courses():
+	limit = 0
+	offset = 0
 	args = {}
 	fields = {'_id': 0, 'terms_offered': 0, 'instructors': 0, 'description': 0, 'term_year': 0, "faculty": 0}
 
 	for key in request.args.to_dict():
-		if key == 'fields':
+		arg = request.args.get(key)
+		if key == 'q':
+			query = {'$regex': '(?i)'}
+			for i in arg.split(' '):
+				print arg
+				query['$regex'] = str(query['$regex']+'.*'+i)
+			print query
+			args['title'] = query
+		elif key == 'limit':
+			limit = int(arg) if arg.isdigit() else arg
+		elif key == 'offset':
+			offset = int(arg) if arg.isdigit() else arg
+		elif key == 'fields':
 			#only show these fields in the response.
-			field_args = request.args.get(key).split(',')
+			field_args = arg.split(',')
 			fields = {'_id': 0}
 			for field_arg in field_args:
 				fields[field_arg]=1
 		elif key == 'unhide-fields':
 			#show the hidden fields of a typical response
-			field_args = request.args.get(key).split(',')
+			field_args = arg.split(',')
 			for field_arg in field_args:
 				try:
 					del fields[field_arg]
 				except:
 					logger.warn('attempted to remove field that was not already remove (possibly doesnt exist)')
-		elif request.args.get(key).isdigit():
-			#if number is a digit, convert it.
-			args[key] = int(request.args.get(key))
 		else:
-			args[key] = request.args.get(key)
-	
-	doc = list(mongo.db['courses'].find(args, fields))
+			in_query = {}
+			in_options = arg.split(',')
+			in_query['$in'] = [int(i) if i.isdigit() else i for i in in_options]
+			args[key] = in_query
+
+	doc = list(mongo.db['courses'].find(args, fields).limit(limit).skip(offset))
 	return Response(json.dumps(doc, indent=4, default=json_util.default), mimetype='application/json')
 	
 
--- populate_course_db.py
+++ populate_course_db.py
@@ -3,161 +3,27 @@ import requests
 import json
 import copy
 
-abrv_to_full_course_subjects = {
-'''
-	ADMB - Administrative Studies Bridging - ( AP ) 
-	ADMS - Administrative Studies - ( AP ) 
-	ANTH - Anthropology - ( AP, GS ) 
-	ARB - Arabic - ( AP ) 
-	ARTH - Art History - ( FA, GS ) 
-	ASL  - Asl  American Sign Language - ( AP ) 
-	BC   - Bethune College - ( SC ) 
-	BCHM - Biochemistry - ( SC ) 
-	BIOL - Biology - ( SC ) 
-	BLIS - International Business Law - ( GS ) 
-	BSUS - Business and Sustainability - ( SB ) 
-	BUEC - Business Economics - ( GL ) 
-	CDIS - Critical Disability - ( GS ) 
-	CDNS - Canadian Studies - ( AP, GL ) 
-	CH   - Chinese - ( AP ) 
-	CHEM - Chemistry - ( GS, SC ) 
-	CLTR - Culture - ( AP ) 
-	CMCT - Communication and Culture - ( GS ) 
-	CMYR - Common Year - ( ED ) 
-	COMN - Communication Studies - ( AP ) 
-	COOP - Cooperative Education - ( SC ) 
-	CRIM - Criminology - ( AP ) 
-	CSE - Computer Science & Engineering - ( GS, LE ) 
-	DANC - Dance - ( FA, GS ) 
-	DEMS - Disaster and Emergency Management - ( GS ) 
-	DRST - Drama Studies - ( GL ) 
-	DVST - Development Studies Graduate Program - ( GS ) 
-	EATS - Earth & Atmos. Science - ( LE ) 
-	ECON - Economics - ( AP, GL, GS, SB ) 
-	EDUC - Education - ( ED, GS ) 
-	EMBA - Executive Masters Business Administration - ( SB ) 
-	EN   - English - ( AP, GL, GS ) 
-	ENG - Engineering - ( LE ) 
-	ENTR - Entrepreneurial Studies - ( SB ) 
-	ENVB - Environmental Biology - ( SC ) 
-	ENVS - Environmental Studies - ( ES ) 
-	ESL  - English As A Second Language - ( AP ) 
-	ESS  - Earth & Space Science - ( GS ) 
-	ETHC - Business Ethics - ( SB ) 
-	EXCH - Exchange - ( SB ) 
-	FACC - Financial Accountability - ( GS ) 
-	FACS - Fine Arts Cultural Studies - ( FA ) 
-	FILM - Film - ( FA, GS ) 
-	FINE - Finance - ( SB ) 
-	FNEN - Financial Engineering - ( SB ) 
-	FNSV - Financial Services - ( SB ) 
-	FR   - French Studies - ( AP ) 
-	FRAN - French Studies/Etudes Francaises  - ( GL ) 
-	FREN - French - ( GS ) 
-	FRLS - Francais Langue Seconde - ( GL ) 
-	FSL - French As A Second Language - ( GL ) 
-	GEOG - Geography - ( AP, GS, SC ) 
-	GER  - German - ( AP ) 
-	GFWS - Gender, Feminist and Womens Studies - ( GS ) 
-	GWST - Gender and Women's Studies - ( AP, GL ) 
-	HIMP - Health Industry Management Program - ( SB ) 
-	HIST - History - ( AP, GL ) 
-	HLST - Health Studies - ( HH ) 
-	HLTH - Health - ( GS ) 
-	HREQ - Human Rights and Equity Studies - ( AP ) 
-	HRM - Human Resources Management - ( AP, GS ) 
-	HUMA - Humanities - ( AP, GL, GS ) 
-	IBUS - International Business - ( SB ) 
-	IHST - Global Health - ( HH ) 
-	ILST - International Studies - ( GL ) 
-	INLE - Inquiries into Learning - ( ED ) 
-	INST - Interdisciplinary Study - ( GS ) 
-	INTE - Conference Interpreting - ( GS ) 
-	INTL - International - ( SB ) 
-	IT   - Italian - ( AP ) 
-	ITEC - Information Technology - ( AP, GS ) 
-	JC - Jamaican Creole - ( AP ) 
-	KAHS - Kinesiology & Health Science - ( GS ) 
-	KINE - Kinesiology & Health Science - ( HH ) 
-	LAL - Linguistics and Applied Linguistics - ( GS ) 
-	LAW  - Law - ( GS ) 
-	LIN - Linguistics and Language Studies - ( GL ) 
-	LING - Linguistics - ( AP ) 
-	LREL - Labour Relations and Employment Law - ( GS ) 
-	MACC - Master of Accounting - ( SB ) 
-	MATH - Mathematics and Statistics - ( GS, SC ) 
-	MFIN - Master of Finance - ( SB ) 
-	MGMT - Management - ( SB ) 
-	MIST - Multicultural and Indigenous Studies - ( AP ) 
-	MKTG - Marketing - ( SB ) 
-	MODR - Modes Of Reasoning - ( AP, GL ) 
-	MSBA - Master of Science in Business Analytics - ( SB ) 
-	MUSI - Music - ( FA, GS ) 
-	NATS - Natural Science - ( GL, SC ) 
-	NURS - Nursing - ( GS, HH ) 
-	OMIS - Operations Management and Information Syste - ( SB ) 
-	ORGS - Organization Studies - ( SB ) 
-	OVGS - Ont.Visiting Graduate Student - ( GS ) 
-	PHIL - Philosophy - ( AP, GL, GS ) 
-	PHYS - Physics and Astronomy - ( GS, SC ) 
-	PIA - Public and International Affairs - ( GS ) 
-	PKIN - Kinesiology & Health Sc Pract - ( HH ) 
-	POLS - Political Science - ( AP, GL, GS ) 
-	POR  - Portuguese - ( AP ) 
-	PPAL - Public Policy Administration & Law - ( GS ) 
-	PPAS - Public Policy and Administration Studies - ( AP ) 
-	PRAC - Practicum - ( ED ) 
-	PROP - Real Property - ( SB ) 
-	PSYC - Psychology - ( GL, GS, HH ) 
-	PUBL - Public Administration - ( SB ) 
-	RYER - Ryerson - ( AP ) 
-	SGMT - Strategic Management - ( SB ) 
-	SLST - Socio-Legal Studies - ( GS ) 
-	SOCI - Sociology - ( AP, GL, GS ) 
-	SOSC - Social Science - ( AP, GL ) 
-	SOWK - Social Work - ( AP, GS ) 
-	SP   - Spanish - ( AP ) 
-	SPTH - Social & Political Thought - ( GS ) 
-	STS  - Science and Technology Studies - ( SC ) 
-	THEA - Theatre - ( FA, GS ) 
-	THST - Theatre Studies - ( GS ) 
-	TRAN - Translation - ( GL, GS ) 
-	URST - Urban Studies - ( AP ) 
-	VISA - Visual Arts - ( FA, GS ) 
-	WMST - Womens Studies - ( GS ) 
-	WRIT - Writing - ( AP ) 
-	YSDN - York/Sheridan Design - ( FA ) 
-'''
+term_offered_template = {'term_year': 0, 'terms': []}
+course_data_template = {
+	'course_code': '',	
+	'course_number': '',
+	'course_subject': '',
+	'title': '',
+	'faculty': '',
+	'year_level': 0,
+	'credit_count': '',
+	'terms_offered': [],
+	'instructors': [],
+	'description': '',
+	'term_year': 0,
 }
 
-if __name__ == '__main__':
-	course_data_template = {
-		'course_code': '',	
-		'course_number': '',
-		'course_subject': '',
-		'title': '',
-		'faculty': '',
-		'year_level': 0,
-		'credit_count': '',
-		'terms_offered': [],
-		'instructors': [],
-		'description': '',
-		'term_year': 0,
-	}
+term_year = 1415
+term = "W or F"
 
-	term_offered_template = {'term_year': 0, 'terms': []}
-
-	#
-	#fo = open("course_list.txt", "rw+")
-	#line = fo.readline()
-
-	term_year = 1415
-	term = "W or F"
-
-
-
-	#print json.dumps(new_data, sort_keys=True, indent=4, separators=(',', ': '))
-	with open('course_list.txt', 'rU') as f:
+def populate_course_db():
+	result_post = []
+	with open('txt_files/course_list.txt', 'rU') as f:
 		for line in f:	
 			if line.strip():
 				new_data = copy.deepcopy(course_data_template)
@@ -181,6 +47,14 @@ if __name__ == '__main__':
 				new_term_data['terms'].append(term)
 
 				new_data['terms_offered'].append(new_term_data)
+				result_post.append(new_data)
+	return result_post
 
-				r = requests.post('http://127.0.0.1:5000/courses/', data=json.dumps(new_data))
+def populate_subject_db():
+	pass
+
+if __name__ == '__main__':
+	#print json.dumps(new_data, sort_keys=True, indent=4, separators=(',', ': '))
+	for i in populate_course_db():
+		r = requests.post('http://127.0.0.1:5000/courses/', data=json.dumps(i))
 		
