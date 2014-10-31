f44c23c70ba7cd74b72bdb327eab3ebe2e1714b1
Maciej Lis <mlisbit@gmail.com>
2014-10-31 14:04:44 -0400

    finished section parsing in native yorku parser

--- dev_helpers/native_parser/native_parser.py
+++ dev_helpers/native_parser/native_parser.py
@@ -4,6 +4,17 @@ import requests_cache
 requests_cache.install_cache(expire_after=30) #not to crash the york 
 from bs4 import BeautifulSoup
 import copy
+import time
+from datetime import datetime, timedelta
+
+##########################################################################################
+##########################################################################################
+##																						##
+## For any one reading this code, and wondering why parsing looks looks so horrible:   	##
+## Part of it's because beautifulsoup4 cannot properly parse York Univsersities site..	##
+##																						##
+##########################################################################################
+##########################################################################################
 
 def get_wosid_and_number():
 	pass
@@ -74,8 +85,8 @@ def stage_2_get_courses_for_subject(course_listing_soup):
 def stage_3_get_sections_from_course(section_listing_soup):
 	section_field = {
 		'description': '',
-		'catagory_code': '',
-		'type': 'LECT',
+		'catagory_number': '',
+		'type': '',
 		'day': '',
 		'start_time': '',
 		'end_time': '',
@@ -83,9 +94,10 @@ def stage_3_get_sections_from_course(section_listing_soup):
 		'location': '',
 		'instructor': '',
 		'section': '',
-		'term': 'F',
-		'is_available': False,
-		'term_year': '1415'
+		'term': '',
+		'term_year': '1415',
+		'notes': '',
+		'building':  ''
 	}
 
 	description = section_listing_soup.select('html body table')[2].select('p')[3].text
@@ -94,17 +106,52 @@ def stage_3_get_sections_from_course(section_listing_soup):
 	tables = section_listing_soup.select('body')[0].findAll('td', {'colspan': '3'})
 	important_table = []
 
+	#getting the section and term letters, placing them in order into an array
+	section_letters = []
+	term_letters = []
+	for i in section_listing_soup.select('body')[0].findAll('td', {'bgcolor': '#CC0000'}):
+		#get the text from the red table heading, and extract the term letter, and section letter.
+		red_table_heading = filter(bool, i.get_text().strip().split(' '))
+		section_letters.append(red_table_heading[3])
+		term_letters.append(red_table_heading[1])
+
+
 	#removes the junk from the previous scrape. 
 	for i in range(1, len(tables), 2):
 		important_table.append(tables[i])
 
 	#get all the important fields.
-	for table in important_table:
-		print table.findAll('td', {'valign': 'TOP'})[0].parent.get_text('|').encode('utf-8')
+	results = []
+	for term_and_section_counter, table in enumerate(important_table):
+		print "---------------" 
+		# this combines the top row of the table with the rows of the table following it,
+		# dont remember why I do this... probably somethign to do without being able to get proper parsing.
+		result_table_text = []
+		result_table_text.append(table.findAll('td', {'valign': 'TOP'})[0].parent.get_text('|').encode('utf-8'))
 		for sub_table in range(0, len(table.findAll('td', {'valign': 'TOP'})[0].parent.find_next_siblings())):
-			print table.findAll('td', {'valign': 'TOP'})[0].parent.find_next_siblings()[sub_table].get_text('|').encode('utf-8')
+			result_table_text.append(table.findAll('td', {'valign': 'TOP'})[0].parent.find_next_siblings()[sub_table].get_text('|').encode('utf-8'))
 
-	all_sections = []
+		#get all the information out of these tables! 
+		all_sections = []
+		for row in result_table_text:
+			split_row = row.split('|')
+			current_section_field = copy.deepcopy(section_field)
+			current_section_field['term'] = term_letters[term_and_section_counter]
+			current_section_field['section'] = section_letters[term_and_section_counter]
+			current_section_field['description'] = description
+			current_section_field['type'] = ''.join([i for i in split_row[0] if not i.isdigit()])
+			current_section_field['day'] = split_row[1]
+			current_section_field['start_time'] = split_row[2]
+			current_section_field['duration'] = split_row[3]
+			current_section_field['location'] = split_row[4].strip()
+			current_section_field['catagory_number'] = split_row[5]
+			current_section_field['instructor'] = split_row[6]
+			current_section_field['notes'] = split_row[7]
+			current_section_field['building'] = ''.join([i for i in current_section_field['location'] if not i.isdigit()])
+			current_section_field['end_time'] = (datetime.strptime(current_section_field['start_time'], "%H:%M") + timedelta(minutes=5)).strftime("%H:%M")
+			all_sections.append(current_section_field)
+
+	
 	return all_sections
 
 def main():
@@ -138,11 +185,15 @@ def main():
 			stage_2_get_courses_for_subject(course_listing_soup)
 	else:
 		the_subject = stage_1_get_all_subjects(subject_soup)[55]
-
+		
 		course_listing_html = requests.post(the_subject['post_url'], headers={}, data=the_subject['subject_data_payload'])
 		course_listing_soup = BeautifulSoup(course_listing_html.text.encode('utf-8'))
 
-		the_course = stage_2_get_courses_for_subject(course_listing_soup)[0]
+		#for i in range(0, stage_2_get_courses_for_subject(course_listing_soup).__len__()):
+		#	print i, stage_2_get_courses_for_subject(course_listing_soup)[i]['course_code']
+		the_course = stage_2_get_courses_for_subject(course_listing_soup)[45]
+		print the_course['course_code']
+		#print the_course['course_code']
 
 		section_listing_html = requests.get(the_course['course_link'])
 		section_listing_soup = BeautifulSoup(section_listing_html.text.encode('utf-8'))
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. openYorkU API documentation master file, created by
+   sphinx-quickstart on Tue Oct  7 20:12:55 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to openYorkU API's documentation!
+=========================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
